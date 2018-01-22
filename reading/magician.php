60be141d586b08a6bc17598dc5dd560dd849e56b
zhixinlin <linzedex@gmail.com>
2018-01-22 12:57:12 +0800

    typo fixes, magician complete

--- reading/magician.php
+++ reading/magician.php
@@ -55,9 +55,13 @@ include ('../inc/header.php');
         <!--Insert Archetype Icon-->
         <img class="img-intext" src="../img/icons/magician.png">
 
-        <p><span aweber-field="name">Friend</span>, there is a strong sense of discipline within you. Unlike other archetypes, you're not afraid of hardwork, and you're not afraid of suffering. Rather, you thrive on it. You have a strange appreciation for pain, but not in the same way as a masochist. Your appreciation for pain stems from associating it with growth and advancement - an appreciation that pushes you forward.</p>
+        <p><span aweber-field="name">Friend</span>, there is a strong <span class="purple">sense of discipline</span> within you. Unlike other archetypes, you're not afraid of hardwork, and you're not afraid of suffering. Rather, you thrive on it.</p> 
+        
+        <p>You have a <span class="purple">strange appreciation for pain,</span> but not in the same way as a masochist. Your appreciation for pain stems from associating it with growth and advancement - an appreciation that pushes you forward.</p>
 
-        <p>People find themselves drawn to you because of your ability to engage in deep, thoughtful conversations. The bonds that you forge with others is often something they've never experienced before. <span aweber-field="name">Friend</span>? Your deep understanding of feelings and approachable appearance makes you a <span class="purple">warm and comforting presence to be around.</span></p>
+        <p>People find themselves drawn to you because of your ability to engage in <span class="purple">deep, thoughtful conversations.</span> The bonds that you forge with others is often something they've never experienced before.</p> 
+        
+        <p>Your deep understanding of feelings and approachable appearance makes you a <span class="purple">warm and comforting presence to be around.</span></p>
 
         <p><span aweber-field="name">Friend</span>, this puts you at a great advantage when it comes to <span class="purple">achieving the things that you set your mind to.</span> You're the type of person who <span class="purple">reaches for the stars</span>, and you have no intention of falling amongst the clouds.</p>
 
@@ -65,7 +69,9 @@ include ('../inc/header.php');
 
         <p>You tend to make an excellent listener, <span aweber-field="name">Friend</span>... But that trait of yours comes from knowing that <span class="purple">information is power.</span> You're slow to reveal your innermost thoughts... You're a firm believer of revealing just enough, but absorbing more than you need.</p>
 
-        <p>The Magician archetype is one that also has the tendency to become <span class="purple">manipulative and scheming.</span> For the most part, you do exercise compassion and your heart's often in the right place. But it's also important to learn to be forgiving and patience, <span aweber-field="name">Friend</span>... But we'll discuss that in greater detail as we progress through this Archetypal Introduction.</p>
+        <p>The Magician archetype is one that also has the tendency to become <span class="purple">manipulative and scheming.</span> For the most part, you do exercise compassion and your heart's often in the right place. But it's also important to learn to be forgiving and patience, <span aweber-field="name">Friend</span>...</p>
+            
+        <p>But we'll discuss that in greater detail as we progress through this Archetypal Introduction.</p>
 
         <h2 class="sub-header">As A Magician, These Are Your Strengths, Tendencies, And Positive Attributes...</h2>
 
@@ -73,50 +79,58 @@ include ('../inc/header.php');
 
         <p>But the learning in question does not refer to academics - not even close. Whether its finding out how certain things are made, or understanding why certain people are charismatic, you're just simply <span class="purple">interested to know everything.</span> That's all driven by your perpetual curiosity, and your talent in comprehending concepts quickly.</p>
 
-        <p>There's something else about your archetype, <span aweber-field="name">Friend</span>...You're also incredibly versatile. You're able to assume different roles according to different situations - very much like an actual magician, who has <span class="purple">numerous tricks</span> hidden in his sleeves. Perhaps the best way to illustrate this is within a team setting - you're able to fill up whatever role is left, and you're able to do it well.</p>
+        <p>There's something else about your archetype, <span aweber-field="name">Friend</span>...You're also incredibly versatile. You're able to assume different roles according to different situations - very much like an actual magician, who has <span class="purple">numerous tricks</span> hidden in his sleeves.</p> 
+        
+        <p>Perhaps the best way to illustrate this is within a team setting - you're able to <span class="purple">fill up whatever role is left,</span> and you're able to do it well.</p>
 
         <p><span aweber-field="name">Friend</span>, based on the strengths of your archetype, you're an individual who's highly observant, and incredibly resourceful.</p>
 
         <!--Insert Strength Image-->
         <img class="img-intext" src="../img/strength.png">
 
-        <p><span aweber-field="name">Friend</span>, as a Magician, you're actually a lot more <span class="purple">nurturing</span> than you think. You might appear to be a little bit emotionally detached on the outside, but when you're alone, you're actually someone who's <span class="purple">warm, caring, and tender.</span> <span class="purple"> And that translates into how well you're able to connect with others - an indicator of emotional stability.</p>
+        <p><span aweber-field="name">Friend</span>, as a Magician, you're actually a lot more <span class="purple">nurturing</span> than you think. You might appear to be a little bit emotionally detached on the outside, but when you're alone, you're actually someone who's <span class="purple">warm, caring, and tender.</span></p>
+            
+        <p>And that translates into how well you're able to connect with others - an indicator of <span class="purple">emotional stability.</span></p>
 
-        <p>Apart from that, <span aweber-field="name">Friend</span>, you seem have a discnering understanding of what it means to love - one that adopts a more philosophical point of view, and also, one that grants you special talents when it comes to <span class="purple">empathy and affinity.</span></p>
+        <p>Apart from that, <span aweber-field="name">Friend</span>, it's likely that others often come to you for <span class="purple">guidance and advice...</span> That's because as a Magician, you're meticulous when it comes to examining the details of any given situation... Allowing you to <span class="purple">determine underlying causes</span> that are unbeknownst to others.</p>
 
         <p>In terms of your behaviour around others, you tend to be a little bit more reserved, especially in larger groups. But, you excel impressively in when it comes to one-on-one interactions. That's where you feel the most <span class="purple">comfortable</span>, knowing that your voice can be heard, and you can forge a <span class="purple">deeper, more personal connection.</span></p>
 
-        <p>You see, <span aweber-field="name">Friend</span>, your aura is one that's <span class="purple">genuine and flirtatious</span>. Your laughter is infectious, and you're simply able to earn the trust of others relatively easily.</p>
+        <p><span aweber-field="name">Friend</span>, while you do enjoy using your talents to <span class="purple">help others</span>, you grow irritable when they refuse to listen. You must always remember not to allow the choices of others to affect your state.</p>
 
-        <h2 class="sub-header">Unfortunately, Not All Is <span class="purple">Smooth-Sailing</span> For The Romantic Magician, <span aweber-field="name">Friend</span></h2>
+        <h2 class="sub-header">Unfortunately, Not All Is <span class="purple">Smooth-Sailing</span> For The Magnificent Magician, <span aweber-field="name">Friend</span></h2>
 
         <p>This portion of your Archetypal Introduction does not seek to scare you, <span aweber-field="name">Friend</span>... But it's best that you be made aware of the <span class="purple">potential challenges</span> that lie ahead of you - or perhaps you've encountered some of these challenges already.</p>
 
         <img class="img-intext" src="../img/sailing.png">
 
-        <p>However, I have no doubt that with your <span class="purple">strenghts and personality traits, </span>, you will overcome all that stands in your way, <span aweber-field="name">Friend</span>.</p>
+        <p>However, I have no doubt that with your <span class="purple">Archetypal strengths and personality traits, </span>, you will overcome all that stands in your way, <span aweber-field="name">Friend</span>.</p>
 
-        <p>The Lover archetype tends to struggle with <span class="purple">its need to be accepted.</span> And that drives you to resort to extensive measures to impress others, <span aweber-field="name">Friend</span>. And that's also indicative of a deeper fear that resides within you, <span aweber-field="name">Friend</span>... The fear of <span class="purple">being rejected, or alone.</span></p>
+        <p>The Magician archetype tends to struggle with <span class="purple">its pride and expectations of others.</span> And that often drives you to be dismissive, critical, and overly serious.</p>
+            
+        <p>But that's also indicative of a deeper fear that resides within you, <span aweber-field="name">Friend</span>... The fear of <span class="purple">failing others... And failing yourself</span></p>
 
-        <p><span aweber-field="name">Friend</span>, as a Lover, it's likely that you feel a void deep within you... A sense of <span class="purple">emptiness or purposeless.</span> Perhaps that could be from a <span class="purple">lack of self-confidence, or a lack of self-esteem,</span> which is why you tend to place so much emphasis on your relationships with others, and tend to <span class="purple">neglect the relationship that you have with yourself.</span></p>
+        <p><span aweber-field="name">Friend</span>, as a Magician, your expectations are often set far too high. At times, your optimistic standards can be an excellent attribute - pushing you to greater heights...</p> 
+        
+        <p>But that's not always the case. There are instances where you, or others, might fall short - and that's when you tend to experience <span class="purple">frustration, disappointment, and helplessness.</span></p>
+        
+        <p>You must understand that <span class="purple">life has so much more to offer,</span> and people are not determined by their ability to meet your expectations - much less yourself. It will be impossible to avoid such setbacks entirely, but how you handle these upsets will determine the <span ckass="purple">destination of your journey.</span></p>
 
-        <p><span aweber-field="name">Friend</span>, do not be alarmed - this is all part and parcel of what it means to be a <span class="purple">Lover</span>. And with your Archetypal Introduction, all of that will soon change.</p>
+        <p><span aweber-field="name">Friend</span>, do not be alarmed - this is all part and parcel of what it means to be a <span class="purple">Magician</span>. And with your Archetypal Introduction, all of that will soon change.</p>
 
         <p>The sooner you're aware of your struggles and weaknesses, the sooner you'll be able to experience the <span class="purple">wonders and greatness</span> that it has to offer.  working on that aspect of your personality will, in fact, reveal a part of your archetype that you never knew existed.</p>
-
-        <p>Based on your other traits, <span aweber-field="name">Friend</span>, you also seem to struggle with <span class="purple">setting yourself up for disappointments</span>. You have the tendency to expect a little too much for others, and you end up feeling dejected if your expectations are not met.</p>
 
         <p><span aweber-field="name">Friend</span>, it's important to understand that all archetypes has its fair share of <span class="purple">weaknesses and challenges</span>. Now that you're aware of them, you'll be able to pay attention to them, and then improve on them.</p>
 
         <h2 class="sub-header"><span aweber-field="name">Friend</span>, As A Magician, This Is Your Calling...</h2>
 
-        <p><span aweber-field="name">Friend</span>, you’re at your happiest when you’re in a committed relationship; especially with someone who treasures you, nurtures you, and loves you for exactly who you are. Your <span class="purple">faithfulness and trust</span> are unquestionable, but if your trust is ever broken, you find it almost impossible to rebuild it.</p>
+        <p><span aweber-field="name">Friend</span>, While others seem to find it difficult to even understand the fundamentals of a new skill, you’re able to <span class="purple">attain mastery</span> over anything you set your mind to. In a way, your greatest strength is your ability to learn.</p>
 
-        <p>However, associating yourself too much with that one single aspect can make you <span class="purple">lost sight of who you truly are.</span> There is so much more to the Lover archetype, <span aweber-field="name">Friend</span>. You tend to associate the successes and turmoils of your relationships with your self-worth.
+        <p>You have a insatiable thirst for knowledge - constantly sourcing for new skills and learning opportunities to add to your already impressive <span class="purple">arsenal of competencies.</span></p>
 
-        <p>However, as mentioned earlier in this Archetypal Introduction, your romantic life is not what defines you as a Lover... And in due time, you will come to understand that your <span class="purple">fears of rejection</span> are binding chains preventing you from being your true and natural self.
+        <p>With those attributes in mind, you will certainly find fulfillment and enjoyment when you engage yourself in a career that allows you to assume a <span class="purple">variety of roles...</span> A position that forces you to apply yourself and your different talents.</p>
 
-        <p><span aweber-field="name">Friend</span>, you create so much value for the people around you, and the world. You have so much to offer, and your path to fulfillment rests upon the valuable connections that you forge, and your ability to spread love and joy to others.</p>
+        <p><span aweber-field="name">Friend</span>, you have the capacity to perform great feats in your lifetime. But you must remember that you cannot do everything on your own - and that's where <span class="purple">forging valuable connections</span> with others will come into play.</p>
 
         <h2 class="sub-header"><span aweber-field="name">Friend</span>, Here's A Glimpse Into Your <span class="purple">Love Compatibility</span> With Other Archetypes...</h2>
 
@@ -124,13 +138,13 @@ include ('../inc/header.php');
 
         <p><span aweber-field="name">Friend</span>, do you see that chart on top? That little chart over there represents the archetypes that you’re most likely to end up with. Or if you’re already in a committed relationship, there’s a really good chance that your spouse is one of those archetypes.</p>
 
-        <p><span aweber-field="name">Friend</span>, the Lover archetype is known to be <span class="purple">loyal, passionate, and committed</span> in a relationship... You're giving and sacrificial - a partner than anyone would want.</p>
+        <p><span aweber-field="name">Friend</span>, the Magician archetype is known to be <span class="purple">faithful, tender, and devoted</span> in a relationship... You're giving and understanding - attributes than any lover would cherish.</p>
 
-        <p>You're not the sort to encounter difficulties when it comes to finding romantic interests - you're <span class="purple">attractive to most,</span> and you're a wonderful person to be around.</span> You do, however, tend to be a little bit careless in your choice of partners.</p>
+        <p><span aweber-field="name">Friend</span>, as much as you dislike to admit it, there is a strong yearning for romance that resides within you. And that's supported by your <span class="purple">faithfulness and committment</span> in a relationship.</p>
 
-        <p>More often than not, your trust is betrayed and your love is taken advantage of, <span aweber-field="name">Friend</span>. But as long as you continue to work on <span class="purple"your relationship with yourself,</span> there is no doubt that a rewarding romantic life will be waiting right around the corner for you - perhaps in the form of your current partner, or a potential suitor.</p>
+        <p>However, when you're focused too much on your relationship, you tend to become <span class="purple">manipulative of your partner</span> - often trying to influence him or her to change his or her perspective and beliefs. It's not that mean you to, it's just simply habitual - a part of your subconscious.</p>
 
-        <p>In the presence of your partner, you wear your heart on your sleeve and you're not afraid to <span class="purple">ask for what you want.</span> You must, however, remember that you do tend to become a little bit <span class="purple">possessive.</span> In order to forge stronger relations with your partner, you must learn to let go and give both of you space to breathe.</p>
+        <p>In the presence of your partner, you can be a little bit distant when your mind's distracted - and no matter how you try to hide it, people will always notice that about you. In order to forge stronger relations with your partner, you must learn to be both <span class="purple">physically and emotionally present.</span></p>
 
         <p>But don’t forget, <span aweber-field="name">Friend</span>, how you perceive yourself <span class="purple">should not be determined by your love life.</span> Romance is merely one, single aspect of life - not the only. It's always best to focus on your own personal growth, because that's the path that you must embark on to lead a rewarding and fulfilled life.</p>
     </div>
